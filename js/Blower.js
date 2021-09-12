e6fdf0028793ea3a33c4ed8720a4d671fc17d276
aarya0208 <86306394+aarya0208@users.noreply.github.com>
2021-09-12 13:28:58 +0530

    Add files via upload

---
+++ js/Blower.js
@@ -0,0 +1,25 @@
+class Blower {
+    constructor(x, y, w, h) {
+      let options = {
+       isStatic : true
+      };
+  
+      this.body = Bodies.rectangle(x, y, w, h, options);
+      this.w = w;
+      this.h = h;
+      this.color = color;
+
+      World.add(world, this.body);
+    }
+  
+    show() {
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rectMode(CENTER);
+      fill("#8d6e63");
+      rect(0, 0, this.w, this.h);
+      pop();
+    }
+  }
